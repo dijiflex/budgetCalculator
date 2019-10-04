b3e9f383ae02b1e93b5a70a589c6e2f03d10b60c
FELIX OMUOK <dijiflex@gmail.com>
2019-10-04 17:01:09 +0300

    Updated Dare Funtion and dislayed the DATE on the UI

--- app.js
+++ app.js
@@ -165,7 +165,8 @@ var UIConntroller = (function () {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
 
     };
 
@@ -286,6 +287,20 @@ var numSplit,int,dec;
             });
         },
 
+        displayMonth: function () {
+            var now,year,months;
+             now = new Date();
+
+             year = now.getFullYear();
+
+             months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+             month = now.getMonth();
+             document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+
+
+            
+        },
+
 
         getDOMstrings: function () {
             return DOMstrings;
@@ -398,6 +413,7 @@ var controller = (function (budgetCtrl, UICtrl) {
                 percentege: 0
             });
             setupEventLinstners();
+            UICtrl().displayMonth();
 
         }
     }
